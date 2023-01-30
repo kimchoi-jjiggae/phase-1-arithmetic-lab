3224eae5855b6ff6e4b70d0b2bdb0e05349525bc
Michelle <choi.michelle.12@gmail.com>
2023-01-29 21:25:45 -0500

    passes tests

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+// multiply
+const num1 = 62/4;
+const num2 = 4;
+let multiply = num1*num2;
+
+// random
+let random = Math.ceil(Math.random()*10);
+
+// mod
+const num3 = 14;
+const num4 = 10;
+let mod = num3%num4;
+
+// max
+let max = Math.max(1, 3, 20);
